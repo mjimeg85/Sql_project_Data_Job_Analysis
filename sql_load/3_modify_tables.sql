bad942d33c37e81d65f91aae800a16bd7a775700
mjimeg85 <mjimeg85@gmail.com>
2024-08-22 22:55:49 +0200

    upload sql files

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,16 @@
+
+COPY company_dim
+FROM '/Users/jimenagomez/Sql_project_Data_Job_Analysis/csv_files/company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM '/Users/jimenagomez/Sql_project_Data_Job_Analysis/csv_files/skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM '/Users/jimenagomez/Sql_project_Data_Job_Analysis/csv_files/job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM '/Users/jimenagomez/Sql_project_Data_Job_Analysis/csv_files/skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
